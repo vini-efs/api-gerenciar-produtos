2a8c8a3d9ef0fb6b5a871632dbf7105f70806569
vini-efs <viniciuseduardo904@gmail.com>
2025-04-16 11:01:18 -0300

    second commit

---
+++ src/main/java/dev/viniefs/GerenciadorProdutos/GerenciadorProdutosApplication.java
@@ -0,0 +1,13 @@
+package dev.viniefs.GerenciadorProdutos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GerenciadorProdutosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GerenciadorProdutosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/viniefs/GerenciadorProdutos/GerenciadorProdutosApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.viniefs.GerenciadorProdutos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GerenciadorProdutosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
