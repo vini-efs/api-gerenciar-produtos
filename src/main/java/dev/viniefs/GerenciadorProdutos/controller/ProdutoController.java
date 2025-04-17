8580ce186f61f1d6e585d4800bf98456c0242fd5
vini-efs <viniciuseduardo904@gmail.com>
2025-04-16 22:28:38 -0300

    Cria as rotas para POST, GET e DELETE

--- src/main/java/dev/viniefs/GerenciadorProdutos/controller/ProdutoController.java
+++ src/main/java/dev/viniefs/GerenciadorProdutos/controller/ProdutoController.java
@@ -1,15 +1,39 @@
 package dev.viniefs.GerenciadorProdutos.controller;
 
+import dev.viniefs.GerenciadorProdutos.model.ProdutoModel;
+import dev.viniefs.GerenciadorProdutos.service.ProdutoService;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
-@RequestMapping("/produto")
+@RequestMapping("/produtos")
 public class ProdutoController {
+
+    private ProdutoService produtoService;
+
+    public ProdutoController(ProdutoService produtoService) {
+        this.produtoService = produtoService;
+    }
 
     @GetMapping("/teste")
     public String testeProduto() { return "Testando o controller"; }
+
+    @PostMapping("/criar")
+    public ProdutoModel criarProduto(@RequestBody ProdutoModel produto) {
+        return produtoService.criarProduto(produto);
+    }
+
+    @GetMapping("/listar")
+    public List<ProdutoModel> listarProdutos() {
+        return produtoService.listarProdutos();
+    }
+
+    @DeleteMapping("/deletar/{id}")
+    public void deletarProduto(@PathVariable Long id) {
+        produtoService.deletarProduto(id);
+    }
+
 
 }
