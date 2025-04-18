906e54d482fb3e8c98b185953d7ab5ee74937598
vini-efs <viniciuseduardo904@gmail.com>
2025-04-18 12:42:59 -0300

    Cria as rotas de POST, GET e DELETE para a classe categoria

--- src/main/java/dev/viniefs/GerenciadorProdutos/controller/CategoriaController.java
+++ src/main/java/dev/viniefs/GerenciadorProdutos/controller/CategoriaController.java
@@ -1,15 +1,38 @@
 package dev.viniefs.GerenciadorProdutos.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import dev.viniefs.GerenciadorProdutos.model.CategoriaModel;
+import dev.viniefs.GerenciadorProdutos.service.CategoriaService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/categoria")
 public class CategoriaController {
 
+    private CategoriaService categoriaService;
+
+    public CategoriaController(CategoriaService categoriaService) {
+        this.categoriaService = categoriaService;
+    }
+
     @GetMapping("/teste")
     public String testeCategoria() {
         return "Testando controller da categoria";
+    }
+
+    @PostMapping("/criar")
+    public CategoriaModel criarCategoria(@RequestBody CategoriaModel novaCategoria) {
+        return categoriaService.criarCategoria(novaCategoria);
+    }
+
+    @GetMapping("/listar")
+    public List<CategoriaModel> listarCategoria() {
+        return categoriaService.listarCategoria();
+    }
+
+    @DeleteMapping("/deletar/{id}")
+    public void deletarCategoria(@PathVariable Long id) {
+        categoriaService.deletarCategoria(id);
     }
 }
