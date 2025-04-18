7f883918ec55a5ab30b5718f0ded35ca09c14593
vini-efs <viniciuseduardo904@gmail.com>
2025-04-18 19:02:49 -0300

    Cria a camada de exception para lidar com erros globais e implementa para lidar com o erro de validação

---
+++ src/main/java/dev/viniefs/GerenciadorProdutos/exception/GlobalHandlerExcpetion.java
@@ -0,0 +1,26 @@
+package dev.viniefs.GerenciadorProdutos.exception;
+
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestControllerAdvice
+public class GlobalHandlerExcpetion {
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<Map<String, String>> handlerValidationErrors(MethodArgumentNotValidException ex) {
+        Map<String, String> errors = new HashMap<>();
+        ex.getBindingResult().getFieldErrors()
+                .forEach(error -> errors.put(error.getField(), error.getDefaultMessage())
+                );
+
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                .body(errors);
+    }
+
+}
