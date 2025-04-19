c15441e0690aa72ab50df94be0bd1b4560f41074
vini-efs <viniciuseduardo904@gmail.com>
2025-04-19 11:45:29 -0300

    Trata melhor as respostas do DELETE e adiciona rota GET para listar a categoria por id

--- src/main/java/dev/viniefs/GerenciadorProdutos/controller/CategoriaController.java
+++ src/main/java/dev/viniefs/GerenciadorProdutos/controller/CategoriaController.java
@@ -32,8 +32,24 @@ public class CategoriaController {
         return ResponseEntity.ok(listaDasCategorias);
     }
 
+    @GetMapping("listar/{id}")
+    public ResponseEntity<String> listarPorId(@PathVariable Long id) {
+        CategoriaModel categoriaPorId = categoriaService.listarCategoriaPorId(id);
+        if (categoriaPorId != null) {
+            return ResponseEntity.ok("Categoria encontrada: " + categoriaPorId.getNome());
+        }
+        return ResponseEntity.status(HttpStatus.NOT_FOUND)
+                .body("Categoria com ID " + id + " não foi encontrado!");
+    }
+
     @DeleteMapping("/deletar/{id}")
-    public void deletarCategoria(@PathVariable Long id) {
-        categoriaService.deletarCategoria(id);
+    public ResponseEntity<String> deletarCategoria(@PathVariable Long id) {
+        CategoriaModel categoria = categoriaService.listarCategoriaPorId(id);
+        if (categoria != null) {
+            categoriaService.deletarCategoria(id);
+            return ResponseEntity.ok("Categoria deletada com sucesso!");
+        }
+        return ResponseEntity.status(HttpStatus.NOT_FOUND)
+                .body("Categoria com ID " + id + " não pôde ser deletada, pois não existe!");
     }
 }
